5529d83c6b787fc752ed5d5ca77c90a676979250
Ysaac Andujar <78627655+YsaacAndujar@users.noreply.github.com>
2022-10-20 10:07:54 -0400

    base

---
+++ src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="topnav">
+        <router-link :to="{name:'home'}">Home</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'NavBar'
+    }
+</script>
+
+<style scoped>
+
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
